91eea37bb70a77c42b8b1ec745170ebe03807741
Tarkan Al-Kazily <tarkan.alkazily@gmail.com>
2024-05-05 09:09:42 -0700

    Move InquiryRequest to common file

--- logue/sdk1.py
+++ logue/sdk1.py
@@ -1,35 +1,7 @@
 # Copyright 2024 Tarkan Al-Kazily
 
 import logue.target
-
-
-class InquiryRequest(logue.target.LogueMessage):
-    """
-     DEVICE INQUIRY MESSAGE REQUEST
-    +---------+------------------------------------------------+
-    | Byte[H] |    Description                                 |
-    +---------+------------------------------------------------+
-    |   F0    | Exclusive Status                               |
-    |   7E    | Non Realtime Message                           |
-    |   nn    | MIDI Channel (Device ID)                       |
-    |   06    | General Information                            |
-    |   01    | Identity Request                               |
-    |   F7    | END OF EXCLUSIVE                               |
-    +---------+------------------------------------------------+
-    """
-
-    def __init__(self, channel: int):
-        """
-        Args:
-            channel: 1-16 MIDI channel ID
-        """
-        super().__init__(data=[0x7E, channel - 1, 0x06, 0x01])
-        self.channel = channel
-
-    @classmethod
-    def from_message(cls, message):
-        channel = message.data[1] + 1
-        return InquiryRequest(channel)
+from logue.common import InquiryRequest
 
 
 class InquiryResponse(logue.target.LogueMessage):
--- logue/sdk2.py
+++ logue/sdk2.py
@@ -1,37 +1,9 @@
 # Copyright 2024 Tarkan Al-Kazily
 
 import logue.target
+from logue.common import InquiryRequest
 
 KORG_ID = 0x42
-
-
-class InquiryRequest(logue.target.LogueMessage):
-    """
-     DEVICE INQUIRY MESSAGE REQUEST
-    +---------+------------------------------------------------+
-    | Byte[H] |    Description                                 |
-    +---------+------------------------------------------------+
-    |   F0    | Exclusive Status                               |
-    |   7E    | Non Realtime Message                           |
-    |   nn    | MIDI Channel (Device ID)                       |
-    |   06    | General Information                            |
-    |   01    | Identity Request                               |
-    |   F7    | END OF EXCLUSIVE                               |
-    +---------+------------------------------------------------+
-    """
-
-    def __init__(self, channel: int):
-        """
-        Args:
-            channel: 1-16 MIDI channel ID
-        """
-        super().__init__(data=[0x7E, channel - 1, 0x06, 0x01])
-        self.channel = channel
-
-    @classmethod
-    def from_message(message):
-        channel = message.data[1] + 1
-        return InquiryRequest(channel)
 
 
 class InquiryResponse(logue.target.LogueMessage):
